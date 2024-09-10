f23a27ad9a64f67e63cb36e72c28ca7a4f4cb01d
Mike Goelzer <mikegoelzer@goelzer.org>
2024-09-10 14:02:13 -0600

    kicad sym editor default supports usual fields

--- scripts/kicad_sym_edit.py
+++ scripts/kicad_sym_edit.py
@@ -21,6 +21,36 @@ class Node:
         self.attributes = attributes or []
         self.children = children or []
 
+class ExcelPasteValues:
+    def __init__(self):
+        self.mpn = ""
+        self.digikey = ""
+    def __str__(self):
+        return f"{self.mpn}\t❌\t{self.digikey}"
+    def __setitem__(self, key, value):
+        if key.lower() == "mpn":
+            setattr(self, "mpn", value)
+        elif key.lower() == "digikey":
+            setattr(self, "digikey", value)
+        else:
+            # silently ignore properties we don't care about for Excel
+            pass
+    def __getitem__(self, key):
+        return getattr(self, key) if key in ["mpn", "digikey"] else None
+    def set_macos_pboard(self):
+        # Fill macOS pasteboard with tab-separated values for easy pasting into Excel
+        try:
+            import subprocess
+            pboard_content = f"{self}"
+            subprocess.run(['pbcopy'], input=pboard_content.encode('utf-8'), check=True)
+            return True
+        except OSError as e:
+            # silently ignore as this is probably not macOS
+            pass
+        except Exception as e:
+            print(f"😰 {COLOR_YELLOW}warning: failed to copy Excel values to clipboard:{COLOR_RESET} {COLOR_RED}{str(e)}{COLOR_RESET}")
+        return False
+
 def parse_kicad_sym(file_content):
     def parse_node(start, depth=0):
         i = start
@@ -96,7 +126,7 @@ def parse_kicad_sym(file_content):
     return root
 
 # Example usage:
-content = """
+kontent = """
 (kicad_symbol_lib
     (version 20231120)
     (generator "kicad_symbol_editor")
@@ -135,21 +165,30 @@ def print_tree(node, indent=""):
     for child in node.children:
         print_tree(child, indent + "  ")
 
-def find_node_of_name(node, node_name, first_or_last="first"):
+def find_nodes_of_name(node, node_name, first_last_or_all="first"):
     """
-    Find the first or last node of a given name in the tree.
+    Find the first node, or last node, or all nodes of a given name in the tree.
     :param node: node to start searching from (e.g., root node)
     :param node_name: string name of the node to find
-    :param first_or_last: "first" or "last"
-    :return: The first or last node of the given name.
+    :param first_last_or_all: "first", "last", or "all"
+    :return: An array containing either (1) only the first node, (2) only the last node, or (3) 
+    all the nodes of the given name.
     """
+    all_nodes_of_name = []
     last_node_of_name = None
     for child in node.children:
         if child.name == node_name:
             last_node_of_name = child
-            if first_or_last == "first":
-                return child
-    return last_node_of_name
+            if first_last_or_all == "first":
+                return [child]
+            elif first_last_or_all == "all":
+                all_nodes_of_name.append(child)
+    if first_last_or_all == "last":
+        return [last_node_of_name]
+    elif first_last_or_all == "all":
+        return all_nodes_of_name
+    else:
+        raise Exception(f"invalid value for first_last_or_all: '{first_last_or_all}'")
 
 def find_new_property_insertion_point(file_content):
     """
@@ -162,13 +201,24 @@ def find_new_property_insertion_point(file_content):
       updated_content = file_content[:ins_pt] + '\n' + NEW_PROPERTY_BLOCK_STRING + file_content[ins_pt:]
     """
     root = parse_kicad_sym(file_content)
-    first_symbol_node = find_node_of_name(root, "symbol", "first")
-    last_property = find_node_of_name(first_symbol_node, "property", "last")
+    first_symbol_node = (find_nodes_of_name(root, "symbol", "first"))[0]
+    last_property = (find_nodes_of_name(first_symbol_node, "property", "last"))[0]
     if last_property:
         # print(f"Last property: {last_property.start}-{last_property.end} with whitespace prefix: '{last_property.whitespace_prefix}'")
         return last_property.end+1, last_property.whitespace_prefix, last_property.whitespace_unit
     else:
         raise Exception("no property nodes found")
+    
+def get_all_property_names(file_content):
+    """
+    Get all property names from the file content
+    :param file_content: string containing .kicad_sym file content as obtained from file.read()
+    :return: list of property names
+    """
+    root = parse_kicad_sym(file_content)
+    first_symbol_node = find_nodes_of_name(root, "symbol", "first")[0]
+    all_property_nodes = find_nodes_of_name(first_symbol_node, "property", "all")
+    return [node.attributes[0] for node in all_property_nodes]
 
 def make_property(name, value, whitespace_prefix, whitespace_unit):
     return f'''{whitespace_prefix}(property "{name}" "{value}"
@@ -181,65 +231,106 @@ def make_property(name, value, whitespace_prefix, whitespace_unit):
 {whitespace_prefix + whitespace_unit})
 {whitespace_prefix})'''
 
+def update_file(file_path, updated_content, dry_run=False, make_backup=True):
+    if dry_run:
+        print(updated_content)
+        print(f"\n{COLOR_BOLD}DRY RUN - FILE NOT MODIFIED{COLOR_RESET}")
+    else:
+        if make_backup:
+            # move the old file to temp dir
+            import tempfile
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.kicad_sym') as temp_file:
+                temp_file.write(updated_content.encode('utf-8'))
+                temp_file_path = temp_file.name
+
+        # write the updated content to the file
+        with open(file_path, 'w') as file:
+            file.write(updated_content)
+        
+        if make_backup:
+            print(f"💾 Original file saved to {COLOR_BOLD}{temp_file_path}{COLOR_RESET}")
+
+def fixup_digikey_url(s):
+    if not s.startswith(("http://", "https://")):
+        return s, False
+    else:
+        # Strip out any "?s=..." part at the end of the URL
+        valid_digikey_url = s.split('?')[0]
+        return valid_digikey_url, True
+
 def main():
     import os
     import argparse
-    parser = argparse.ArgumentParser(description="Add a property in a .kicad_sym file", exit_on_error=False)
+    parser = argparse.ArgumentParser(description="Add standard (or optionally custom) property/ies to a .kicad_sym file", exit_on_error=False)
     parser.add_argument("FILE", help=".kicad_sym file to modify")
     parser.add_argument("-d", "--dry-run", help="just write to stdout without modifying FILE", action="store_true")
-    parser.add_argument("-p", "--parse", help="print the parse tree FILE (unmodified)", action="store_true")
+    parser.add_argument("-p", "--show-parse", help="print the parse tree FILE (unmodified)", action="store_true")
+    parser.add_argument("-c", "--custom-names", help="prompt for custom property names instead of assuming standard properites: Datasheet, Digikey, Mfgr and MPN", action="store_true", default=False)
     try:
         args = parser.parse_args()
         if not args.FILE or not os.path.exists(args.FILE):
             print(f"{COLOR_RED}File not found: {args.FILE}{COLOR_RESET} (-h for help)")
             return 1
-    except argparse.ArgumentError as e:
+    except argparse.ArgumentError:
         return 1
 
     # read the file
     with open(args.FILE, 'r') as file:
         content = file.read()
     
+    # parse the file and verify that it is a valid kicad_symbol_lib
     root = parse_kicad_sym(content)
-    if args.parse:
+    if not root or not root.name == "kicad_symbol_lib":
+        print(f"{COLOR_RED}abort: {args.FILE} is not a valid .kicad_sym file{COLOR_RESET}")
+        return 1
+
+    if args.show_parse:
         # print the entire file tree
         print_tree(root)
-        return
+        print()
     else:
         # modify the file with backup to temp file
-
-        # check if the root node is a kicad_symbol_lib
-        if not root or not root.name == "kicad_symbol_lib":
-            print(f"{COLOR_RED}abort: {args.FILE} is not a valid .kicad_sym file{COLOR_RESET}")
-            return 1
-        
-        # prompt the user for the new property name and value
-        new_prop_name = input("Enter new property name  > ")
-        new_prop_value = input("Enter new property value > ")
-
-        # do the insertion of the new node NewProperty/NewValue
-        ins_pt, whitespace_prefix, whitespace_unit = find_new_property_insertion_point(content)
-        updated_content = content[:ins_pt] + \
-            '\n' + \
-            make_property(new_prop_name, new_prop_value, whitespace_prefix, whitespace_unit) + \
-            content[ins_pt:]
-        
-        if args.dry_run:
-            print(updated_content)
-            print(f"\n{COLOR_BOLD}DRY RUN - FILE NOT MODIFIED{COLOR_RESET}")
+        if not args.custom_names:
+            property_names = ["Datasheet", "Digikey", "Mfgr", "MPN"]
+            excel_pboard = ExcelPasteValues()
         else:
-            # move the old file to temp dir
-            import tempfile
-            with tempfile.NamedTemporaryFile(delete=False, suffix='.kicad_sym') as temp_file:
-                temp_file.write(content.encode('utf-8'))
-                temp_file_path = temp_file.name
+            property_names = [ input("Enter a new property name > ") ]
 
-            # write the updated content to the file
-            with open(args.FILE, 'w') as file:
-                file.write(updated_content)
+        # find the insertion point for the new property block(s)
+        ins_pt, whitespace_prefix, whitespace_unit = find_new_property_insertion_point(content)
+
+        # prompt the user for the new property values
+        new_prop_blocks_string = ""
+        for new_prop_name in property_names:
+            if new_prop_name in [prop.strip('"') for prop in get_all_property_names(content)]:
+                print(f"✋ {COLOR_YELLOW}warning: property '{new_prop_name}' already exists{COLOR_RESET}: skipping")
+                continue
+            new_prop_value = input(f"Enter value for {COLOR_BOLD}{new_prop_name}{COLOR_RESET} (leave blank to skip) > ")
+            if new_prop_value:
+                # special handling for vendor URLs
+                if new_prop_name == "Digikey":
+                    new_prop_value, url_valid = fixup_digikey_url(new_prop_value)
+                    if not url_valid:
+                        print(f"⚠️ {COLOR_YELLOW}warning: skipping invalid url '{new_prop_value}'{COLOR_RESET}")
+                        continue
+                
+                new_prop_blocks_string += '\n' + make_property(new_prop_name, new_prop_value, whitespace_prefix, whitespace_unit)
+                
+                # saving of values for Excel pboard at end
+                excel_pboard[new_prop_name] = new_prop_value
+
+        # do the insertion of the new node(s) and update the file
+        if new_prop_blocks_string:
+            updated_content = content[:ins_pt] + new_prop_blocks_string + content[ins_pt:]
+            update_file(args.FILE, updated_content, args.dry_run)
+            print(f"✅ {COLOR_GREEN}File updated successfully{COLOR_RESET}")
             
-            print(f"{COLOR_GREEN}File updated successfully{COLOR_RESET}")
-            print(f"Original file saved to {COLOR_BOLD}{temp_file_path}{COLOR_RESET}")
+            # Fill macOS pasteboard with tab-separated values for easy pasting into Excel
+            if excel_pboard.set_macos_pboard():
+                print(f"✅ {COLOR_GREEN}Copied pasteable Excel values:{COLOR_RESET} '{excel_pboard}'")
+        else:
+            print(f"🚫 aborting: no new properties to add")
+            return 1
 
 if __name__ == "__main__":
     try:
