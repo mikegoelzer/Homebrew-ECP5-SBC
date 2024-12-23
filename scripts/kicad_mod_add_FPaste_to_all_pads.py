e82a9ad85f8637e80f75535d0e6ac9ec2d7eabc7
Mike Goelzer <mikegoelzer@goelzer.org>
2024-12-23 07:37:43 -0500

    script used to add F.Paste layer to all pads in footprint

---
+++ scripts/kicad_mod_add_FPaste_to_all_pads.py
@@ -0,0 +1,32 @@
+import re
+
+def add_paste_layer(filename):
+    # Read the file
+    with open(filename, 'r') as file:
+        content = file.read()
+
+    # Regular expression to find pad definitions and their layer lists
+    pattern = r'\(layers "F\.Cu" "F\.Mask"\)'
+    
+    # Count original occurrences
+    count = len(re.findall(pattern, content))
+    
+    # Replace each occurrence, adding F.Paste to the layers list
+    modified_content = re.sub(
+        pattern,
+        r'(layers "F.Cu" "F.Paste" "F.Mask")',
+        content
+    )
+
+    # Write the modified content back to the file
+    with open(filename, 'w') as file:
+        file.write(modified_content)
+    
+    # Print the number of modifications
+    print(f"Modified {count} layer definitions")
+    if count != 100:
+        print("Warning: Expected 100 modifications for 100 pads!")
+
+# Use the script
+filename = "../hardware/parts/DF40C_100DS_0_4V_51_/HIROSE_DF40C-100DS-0.4V_51_.kicad_mod"
+add_paste_layer(filename)
